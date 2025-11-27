b89210362a8930e7e4b05b5ef088d83214952e9c
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-11-27 23:26:47 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,3 +14,8 @@ echo "clone vt"
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b rui4-oss ./vendor/realme/RMX3191
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --recurse-submodules https://github.com/yuzumi86/kernel_realme_even-4.19.git -b bpf --depth=1 kernel/realme/RMX3191
+git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-22.1 ./hardware/oplus
+git clone https://github.com/cumaRull/android_hardware_mediatek.git -b lineage-22.1 ./hardware/mediatek
+
+rm -rf ./hardware/mediatek/InCallService
+rm -rf ./hardware/oplus/interfaces/
