0977bd25dd0c32f5c39ac14ad4ee0b9c6fb005cb
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-04 09:52:50 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ rm -rf ./device/realme/RMX3191-kernel
 echo "succesfully"
 
 echo "clone vt"
-git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b rui4-oss ./vendor/realme/RMX3191
+git clone --depth=1 https://github.com/yuzumi86/vendor_realme_RMX3191.git -b rui4-oss ./vendor/realme/RMX3191
 #git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --recurse-submodules https://github.com/yuzumi86/kernel_realme_even-4.19.git -b bpf --depth=1 kernel/realme/RMX3191
 git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-20 ./hardware/oplus
