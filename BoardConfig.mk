c39869bbcc6f68c7532c4b438db7382db6e1175b
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-08 04:19:00 +0000

    prebuilt dtbo
    Signed-off-by: yuzumi <157438148+yuzumi86@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,7 +110,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/mylitle-clang
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
-#BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS=" -fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_LLVM_BINUTILS := false
 
