7d17fa60be82e1c4bc81b0d1f8d335a888470ef3
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-09 05:36:20 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,9 +7,10 @@ rm -rf packages/apps/RealmeParts
 rm -rf packages/apps/RealmeDirac
 
 echo "Cloning Dependencies"
-git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b suki-dev ./kernel/realme/RMX3191
+git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b rui2-backupx ./kernel/realme/RMX3191
 git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b twelve-rui2 ./vendor/realme/RMX3191
+git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b thirteen ./vendor/realme/RMX3191-ims
 git clone --depth=1 https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-20 ./packages/apps/RealmeParts
 echo "Done!"
 
