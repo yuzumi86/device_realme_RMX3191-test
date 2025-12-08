45a21b5831648971dd872bf6ac6be64bda5616c8
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-09 05:34:51 +0700

    Update and rename arrow_RMX3191.mk to lineage_RMX3191.mk

--- lineage_RMX3191.mk
+++ lineage_RMX3191.mk
@@ -19,13 +19,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOs stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX3191/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX3191
+PRODUCT_NAME := lineage_RMX3191
 PRODUCT_DEVICE := RMX3191
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme c25
