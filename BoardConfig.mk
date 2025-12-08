e168efe6b83afa16bc0241c51bde6699c3abd5bd
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-08 04:33:24 +0000

    import lineage matrix
    Signed-off-by: yuzumi <157438148+yuzumi86@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,11 +91,13 @@ BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # Manifest
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/manifests/manifest_nfc.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+$(DEVICE_PATH)/configs/manifests/framework_compatibility_matrix.xml \
+vendor/lineage/config/device_framework_matrix.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
